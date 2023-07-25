1db859f32828094e11aa04088d5762e1e2418b6c
Enzo Spinella <platinamaster5@gmail.com>
2023-07-25 03:39:13 -0300

    Finished

--- script.js
+++ script.js
@@ -1,3 +1,21 @@
+document.addEventListener('DOMContentLoaded', async () => {
+    let res = await fetch("https://api.github.com/users/enzospinella");
+    let json = await res.json();
+    console.log(json)
+
+    let conteudo = document.querySelector("#conteudo")
+    conteudo.innerHTML += "<p><a href='"+json.html_url+"'>" + json.login + "</a></p>"
+    conteudo.innerHTML += "<p>" + json.name + "</p>"
+    conteudo.innerHTML += "<p>" + json.bio + "</p>"
+    let data = json.created_at.split("-");
+    data = data[2].split("T")[0] + "/" + data[1] + "/" + data[0]
+    conteudo.innerHTML += "<p>" + data + "</p>"
+
+    let avatar = document.querySelector("#avatar")
+    avatar.firstElementChild.src = json.avatar_url
+    avatar.lastElementChild.textContent = json.name
+});
+
 function changeDarkLightMode() {
     const button = document.getElementById("dark-light-mode")
     const body = document.body
@@ -39,4 +57,7 @@ function changeDarkLightMode() {
     const titulosCard = document.getElementsByClassName("titulo-card")
     for (let titulo of titulosCard)
         titulo.style.color = "black"
+    const githubItens = document.getElementById("github-desc")
+    for (let item of githubItens.children)
+        item.style.color = "black"
 }
